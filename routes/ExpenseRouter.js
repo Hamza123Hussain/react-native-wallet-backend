acb56232d9d209d43c83218611b912c7fc90f6e2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-07-13 19:45:46 +0500

    Update ExpenseRouter.js

--- routes/ExpenseRouter.js
+++ routes/ExpenseRouter.js
@@ -2,8 +2,10 @@ import { Router } from 'express'
 import { AddTransaction } from '../Controllers/AddTransaction.js'
 import { GetUserTransaction } from '../Controllers/GetUserTransactions.js'
 import { DeleteTransaction } from '../Controllers/DeleteTransaction.js'
+import { GetTransactionSummary } from '../Controllers/GetTransactionSummary.js'
 
 export const TransactionsRouter = Router()
 TransactionsRouter.post('/AddTransaction', AddTransaction)
 TransactionsRouter.get('/GetUserTransactions/:userid', GetUserTransaction)
 TransactionsRouter.delete('/DeleteTransaction/:id', DeleteTransaction)
+TransactionsRouter.get('/TransactionSummary/:userid', GetTransactionSummary)
