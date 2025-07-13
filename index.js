969d781735115188953b0a5117dbfb66f531f645
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-07-13 19:45:41 +0500

    rate limiter middleware called, new data type has been defined

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { sql } from './Config/DBconfig.js'
 import { TransactionsRouter } from './routes/ExpenseRouter.js'
+import { MainRatelimiter } from './Config/Ratelimiter.js'
 
 dotenv.config()
 const App = express()
 
 const PORT = process.env.PORT || 6000
 App.use(express.json())
+App.use(MainRatelimiter)
 async function InitiDB() {
   try {
     await sql`CREATE TABLE IF NOT EXISTS transcations (
           id SERIAL PRIMARY KEY,
           userid VARCHAR(255) NOT NULL,
           title VARCHAR(255) NOT NULL,
-          amount VARCHAR(255) NOT NULL,
+          amount DECIMAL(10,2) NOT NULL,
           category VARCHAR(255) NOT NULL,
           createdat DATE NOT NULL DEFAULT CURRENT_DATE
           )`
