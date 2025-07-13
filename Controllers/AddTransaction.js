a26f084f1d8dcd719665cbf143afcb92fe3c53e6
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-07-13 18:18:52 +0500

    this will allow us to add a new transaction to the database

---
+++ Controllers/AddTransaction.js
@@ -0,0 +1,19 @@
+import { sql } from '../Config/DBconfig.js'
+
+export const AddTransaction = (req, res) => {
+  const { category, amount, title, userid } = req.body
+  try {
+    if (!title || !amount || !category || !userid) {
+      res.status(400).json('Enter All Fields')
+    }
+    const transaction = sql`INSERT INTO transcations(userid,title,amount,category)
+    VALUES (${userid},${title},${amount},${category})
+    RETURNING *
+    `
+    console.log(transaction)
+    res.status(201).json(transaction[0])
+  } catch (error) {
+    console.log('ERROR CREATING THE TRANSACTION', error)
+    res.status(500).json('INTERNAL SERVER ERROR')
+  }
+}
