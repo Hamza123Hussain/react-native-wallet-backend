bfcabfef73e38c3a9a6913b0473a778fbc02674c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-07-13 17:56:09 +0500

    first table has been made in new sql database

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { sql } from './Config/DBconfig.js'
+dotenv.config()
+const App = express()
+
+const PORT = process.env.PORT || 6000
+
+async function InitiDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transcations (
+          id SERIAL PRIMARY KEY,
+          userid VARCHAR(255) NOT NULL,
+          title VARCHAR(255) NOT NULL,
+          amount VARCHAR(255) NOT NULL,
+          category VARCHAR(255) NOT NULL,
+          createdat DATE NOT NULL DEFAULT CURRENT_DATE
+          )`
+    console.log('database has been connected')
+  } catch (error) {
+    console.log('DATABASE HAS NOT BEEN CONNECTED DUE TO ERROR', error)
+    process.exit(1)
+  }
+}
+InitiDB().then(() => {
+  App.listen(PORT, () => {
+    console.log('APP HAS BEEN TURNED ON')
+  })
+})
